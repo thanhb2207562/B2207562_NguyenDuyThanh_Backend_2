e3d84229c7c585429608deebc670c9fe1ffd4740
thanhb2207562 <thanhb2207562@student.ctu.edu.vn>
2025-07-20 01:09:43 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
+const contactRoutes = require('./routes/contact.route');
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/contacts', contactRoutes);
 app.get('/', (req, res) => {
   res.send('WELCOME TO CONTACT BOOK APPLICATION');
 });
